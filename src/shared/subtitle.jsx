787cb41b6ce1d12eda61c19d5727057ffbf3a6e1
rishabh777ry <rishabh777ry@gmail.com>
2024-10-17 15:24:12 +0530

    text style change

--- src/shared/subtitle.jsx
+++ src/shared/subtitle.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const subtitle = ({subtitle}) => {
    return (
-      <h3 style={{ fontFamily: '"Montserrat", sans-serif', fontSize: '1.5rem', color: '#6e7074' }}>
-      {subtitle}
-   </h3>
+       <h3 className='section__subtitle' style={{ fontFamily: '"Montserrat", sans-serif', fontSize: '1.5rem', color: '#6e7074' }}>
+         {subtitle}
+      </h3>
    )
 }
 
