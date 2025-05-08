1e7365f8310de3f0b80d76e84a5d222f8560898a
Rishabh Yadav <87073393+rishabh777ry@users.noreply.github.com>
2025-05-09 00:10:22 +0530

    Update MaintenancePage.jsx
    ..

--- src/MaintenancePage.jsx
+++ src/MaintenancePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MaintenancePage() {
   return (
     <div style={{ textAlign: 'center', padding: '100px', fontFamily: 'Arial' }}>
-      <h1>🚧 Website Under Maintenance 🚧</h1>
+      <h1> Website Under Maintenance </h1>
       <p>We are currently working on updates. Please check back later!</p>
     </div>
   );
