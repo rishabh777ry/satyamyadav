459c19baac32429adc03cb4bc227240cfa8792cb
Rishabh Yadav <87073393+rishabh777ry@users.noreply.github.com>
2025-05-18 17:43:44 +0530

    Update App.js
    back to server

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Layout from './components/Layout/Layout'
 import MaintenancePage from './MaintenancePage';
 
 function App() {
-   // return <Layout />;
-    return (
-    <MaintenancePage />
-  );
+   return <Layout />;
+  //   return (
+  //   <MaintenancePage />
+  // );
 }
 
 export default App;
